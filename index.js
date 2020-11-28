a3da5743e7c3641e2a29c15ff9b4658f627f724f
bellsource <belubascur@gmail.com>
2020-11-28 15:36:19 -0300

    utilizacion del middelware para todas las rutas del servidor

--- index.js
+++ index.js
@@ -7,6 +7,26 @@
  //Creo el servidor
  const server = express();
 
+//Creo funcion que no hace nada sino seteo el middleware
+function loguearRuta(req,res,next){
+    console.log("Esta accediendo a la ruta: " + req.path);
+    next();
+}
+
+//Middelware
+//utilización del middelware a nivel server, en su totalidad
+server.use(loguearRuta);
+
+//para saber si funciona
+server.get("/" , (req,res) => {
+    res.send("esta es una ruta de prueba");
+})
+
+//creo otra ruta
+server.get("/prueba" , (req,res) => {
+    res.send("esta es otra ruta de prueba");
+})
+
  //Setear donde se escucha ese server
  server.listen(3000 , () => {
     console.log("servidor iniciado...");
